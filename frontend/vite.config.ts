413e1ca2ae3535e83663c7898093bc293b278ae0
Heron Amaral <heron.amaral@gmail.com>
2025-07-20 18:20:34 -0300

    feat: add testing setup and initial tests for App component with Vitest and Testing Library

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { configDefaults } from 'vitest/config';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+    exclude: [...configDefaults.exclude, "e2e/*"],
+    coverage: {
+      reporter: ["text", "html"],
+      provider: "v8",
+    },
+  },
+});
